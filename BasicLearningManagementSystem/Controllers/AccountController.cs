e290ad8e81be8c3a26cfe84b3c9f90e53bb21fdf
Faw12699 <fawad.ajmal12699@gmail.com>
2024-04-25 01:38:42 +0500

    Database Management System Lab assignment- (Create and Push repo)

---
+++ BasicLearningManagementSystem/Controllers/AccountController.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BasicLearningManagementSystem.Models;
+using System.Data.SqlClient;
+
+namespace BasicLearningManagementSystem.Controllers
+{
+    public class AccountController : Controller
+    {
+        // GET: Account
+        SqlConnection con = new SqlConnection();
+        SqlCommand cmd = new SqlCommand();
+        SqlDataReader dr;
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        void connectionString()
+        {
+            con.ConnectionString = @"Data Source=DESKTOP-I5537DP\SQLEXPRESS;Initial Catalog=LMSDatabase;Integrated Security=True";
+        }
+        [HttpPost]
+        public ActionResult Verify(Admin admin) 
+        {
+            connectionString();
+            con.Open();
+            cmd.Connection = con;
+            cmd.CommandText = "SELECT * FROM AdminAccount WHERE id= " + admin.id + " AND password= '" + admin.password + "'";
+            dr = cmd.ExecuteReader();
+
+            if (dr.Read())
+            {
+                con.Close();
+                return View("Create");
+            }
+            else
+            {
+                con.Close();
+                return View("Error");
+            }
+        }
+    }
+}
---
+++ BasicLearningManagementSystem/Models/Admin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BasicLearningManagementSystem.Models
+{
+    public class Admin
+    {
+        public int id { get; set; }
+        public string password { get; set; }
+    }
+}
